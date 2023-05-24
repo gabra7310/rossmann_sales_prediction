2c15e3ff046c14d78cf2b8e05edf2bdd5a9f444f
gabra7310 <gabriel.alves995@gmail.com>
2023-05-24 01:39:33 -0300

    Creating telegram bot api

--- api/rossmann_bot.py
+++ api/rossmann_bot.py
@@ -2,8 +2,10 @@ import pandas as pd
 import requests
 import json
 from flask import Flask, request, Response
+import os
 
-TOKEN = 'XXX'
+
+TOKEN = os.getenv('TOKEN')
 
 def send_message(chat_id, text):
     url = 'https://api.telegram.org/bot{}/'.format(TOKEN)
@@ -22,15 +24,19 @@ def load_data(store_id):
     df_test = pd.merge(df10, df_raw_2, how='left', on='Store')
 
     # choose store for prediction
-    df_test = df_test[df_test['Store'] == 22]
+    df_test = df_test[df_test['Store'] == store_id]
 
-    # remove closed days
-    df_test = df_test[df_test['Open'] != 0]
-    df_test = df_test[~df_test['Open'].isnull()]
-    df_test = df_test.drop('Id', axis=1)
+    if not df_test.empty:
+        # remove closed days
+        df_test = df_test[df_test['Open'] != 0]
+        df_test = df_test[~df_test['Open'].isnull()]
+        df_test = df_test.drop('Id', axis=1)
 
-    # convert Dataframe to json
-    data = json.dumps(df_test.to_dict(orient='records'))
+        # convert Dataframe to json
+        data = json.dumps(df_test.to_dict(orient='records'))
+
+    else:
+        data = 'error'
 
     return data
 
@@ -47,20 +53,58 @@ def predict(data):
 
     return d1
 
+def parse_message(message):
+    chat_id = message['message']['chat']['id']
+    store_id = message['message']['text']
+
+    store_id = store_id.replace('/','')
+
+    try:
+        store_id = int(store_id)
+    except ValueError:
+        store_id = 'error'
+
+    return chat_id, store_id
+
 # API initialize
 app = Flask(__name__)
 
-@app.route('/', method=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
+
 def index():
     if request.method == 'POST':
         message = request.get_json()
 
         chat_id, store_id = parse_message(message)
 
+        if store_id != 'error':
+            # loading data
+            data = load_data(store_id)
+
+            if data != 'error':
+                # prediction
+                d1 = predict(data)
+                # calculation
+                d2 = d1[['store', 'prediction']].groupby( 'store' ).sum().reset_index()
+                #for i in range( len( d2 ) ):
+                msg =  'Store Number {} will sell R${:,.2f} in the next 6 weeks'.format(d2.loc[i, 'store'], d2.loc[i, 'prediction']) 
+
+                # send message
+                send_message(chat_id, msg)
+                return Response('Ok', status=200)
+            else:
+                send_message(chat_id, 'Store not available')
+                return Response('Ok', status=200)
+        else:
+            send_message(chat_id, 'Store ID is Wrong')
+            return Response('Ok', status=200)
+
     else:
+        send_message(566118005, "eai porra")
         return '<h1>Rossmann Store Prediction BOT </h1>'
 
 if __name__ == '__main__':
+    port = os.environ.get( 'PORT', 5000 )
     app.run(host='0.0.0.0', port=5000)
 
 #d2 = d1[['store', 'prediction']].groupby( 'store' ).sum().reset_index()
